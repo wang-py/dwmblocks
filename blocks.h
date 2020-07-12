02cd5c1d6cdd4fcda6727011a6efc74437ef4a9e
MillerMC96 <pywang@ucdavis.edu>
2020-07-11 23:28:20 -0700

    no busy waiting

--- blocks.h
+++ blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/.local/bin/statusbar/vpn.sh",					1,		0},
+	{"", "~/.local/bin/statusbar/vpn.sh",					0,		10},
 	
-	{"", "~/.local/bin/statusbar/memory.sh",                        	30,		0},
+	{"", "~/.local/bin/statusbar/memory.sh",                        	30,		2},
 
 //	{"", "~/bin/statusbar/volume",						0,		10},
 
@@ -11,9 +11,9 @@ static const Block blocks[] = {
 	
 //	{"", "~/bin/statusbar/battery",						5,		0},
 
-	{"", "~/.local/bin/statusbar/temp.sh",  				5,		0},
+	{"", "~/.local/bin/statusbar/temp.sh",  				5,		3},
 
-	{"", "~/.local/bin/statusbar/clock.sh",					60,		0},
+	{"", "~/.local/bin/statusbar/clock.sh",					60,		4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
