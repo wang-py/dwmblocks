2b9cf2e069a93b85149d18a026650b5a4f4f3f08
MillerMC96 <pywang@ucdavis.edu>
2020-07-18 11:42:27 -0700

    added volume

--- blocks.h
+++ blocks.h
@@ -1,11 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "~/.local/bin/statusbar/volume.sh",				0,		5},
+
 	{"", "~/.local/bin/statusbar/vpn.sh",					0,		1},
 	
 	{"", "~/.local/bin/statusbar/gpu.sh",					5,		10},
 
-	{"", "~/.local/bin/statusbar/memory.sh",                        	30,		2},
+	{"", "~/.local/bin/statusbar/memory.sh",               	30,		2},
 
 //	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
 	
